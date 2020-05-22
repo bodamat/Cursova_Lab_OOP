da1d161f75f2f44d88974ef9c6479a1d8734b0fe
BodaMat <bomtvv@gmail.com>
2020-05-23 02:08:35 +0300

    add comments for class Iterator

--- Classes/Iterator.cpp
+++ Classes/Iterator.cpp
@@ -1,27 +1,27 @@
 ﻿#include "Iterator.h"
 
-Iterator::Iterator(Group group)
+Iterator::Iterator(Group group) // constructor
 {
 	this->group = group;
 	index = 0;
 }
 
-void Iterator::next()
+void Iterator::next() // next index
 {
 	index++;
 }
 
-void Iterator::first()
+void Iterator::first() // set to first index
 {
 	index = 0;
 }
 
-bool Iterator::isDone() const
+bool Iterator::isDone() const // is the last index?
 {
 	return index == group.get_student_count();
 }
 
-Student Iterator::currentItem() const
+Student Iterator::currentItem() const // get current value from array
 {
 	return group.studentsArray[index];
 }
--- Classes/Iterator.h
+++ Classes/Iterator.h
@@ -6,10 +6,11 @@ class Iterator
 private:
 	Group group;
 public:
-	int index;
-	Iterator(Group group);
-	void next();
-	void first();
-	bool isDone() const;
-	Student currentItem() const;
+	int index; // array index
+	
+	Iterator(Group group); // constructor
+	void next(); // next index
+	void first(); // set to first index
+	bool isDone() const; // is the last index?
+	Student currentItem() const; // get current value from array
 };
